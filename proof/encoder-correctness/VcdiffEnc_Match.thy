a72570df75616be9314eb88b2e3010e5442c427e
Michael Sproul <michael@sigmaprime.io>
2026-06-28 00:43:34 +1000

    Preserve arbitrary bytes through build index

--- proof/encoder-correctness/VcdiffEnc_Match.thy
+++ proof/encoder-correctness/VcdiffEnc_Match.thy
@@ -4262,6 +4262,270 @@ lemma build_index_fill_loop_preserves_inv_heap_bytes:
   qed
   done
 
+lemma build_index'_preserves_heap_bytes:
+  fixes src buf :: "8 word ptr"
+    and src_len :: "32 word"
+    and head next_arr :: "32 word ptr"
+  assumes src_len_word:
+        "unat src_len < unat (no_entry32 :: 32 word)"
+      and src_valid:
+        "\<And>(off :: 32 word) (st' :: lifted_globals).
+          \<lbrakk>heap_typing st' = heap_typing s; unat off < unat src_len\<rbrakk> \<Longrightarrow>
+          IS_VALID(8 word) st' (src +\<^sub>p uint off)"
+      and head_valid:
+        "\<And>h (st' :: lifted_globals).
+          \<lbrakk>heap_typing st' = heap_typing s; h < hash_size\<rbrakk> \<Longrightarrow>
+          IS_VALID(32 word) st' (head +\<^sub>p int h)"
+      and next_valid:
+        "\<And>p (st' :: lifted_globals).
+          \<lbrakk>heap_typing st' = heap_typing s; p < unat src_len\<rbrakk> \<Longrightarrow>
+          IS_VALID(32 word) st' (next_arr +\<^sub>p int p)"
+      and head_no_alias:
+        "\<And>h bucket. \<lbrakk>h < hash_size; bucket < hash_size; h \<noteq> bucket\<rbrakk> \<Longrightarrow>
+          head +\<^sub>p int h \<noteq> head +\<^sub>p int bucket"
+      and next_no_alias:
+        "\<And>q p. \<lbrakk>q < unat src_len; p < unat src_len; q \<noteq> p\<rbrakk> \<Longrightarrow>
+          next_arr +\<^sub>p int q \<noteq> next_arr +\<^sub>p int p"
+      and next_head_disjoint:
+        "\<And>h p. \<lbrakk>h < hash_size; p < unat src_len\<rbrakk> \<Longrightarrow>
+          head +\<^sub>p int h \<noteq> next_arr +\<^sub>p int p"
+      and head_next_disjoint:
+        "\<And>q bucket. \<lbrakk>q < unat src_len; bucket < hash_size\<rbrakk> \<Longrightarrow>
+          next_arr +\<^sub>p int q \<noteq> head +\<^sub>p int bucket"
+  shows "build_index' src src_len head next_arr \<bullet> s
+    \<lbrace> \<lambda>r t. r = Result () \<and>
+        heap_typing t = heap_typing s \<and>
+        heap_bytes t buf n = heap_bytes s buf n \<rbrace>"
+proof -
+  let ?src_bytes = "heap_bytes s src (unat src_len)"
+  have hashes:
+    "build_index_hashes_ok s src src_len ?src_bytes"
+    by (rule build_index_hashes_okI[OF src_valid])
+  have head_valid_word:
+    "\<And>idx. idx < (0x10000 :: 32 word) \<Longrightarrow>
+      IS_VALID(32 word) s (head +\<^sub>p uint idx)"
+  proof -
+    fix idx :: "32 word"
+    assume idx_lt: "idx < (0x10000 :: 32 word)"
+    have idx_nat_lt: "unat idx < hash_size"
+      using idx_lt by (simp add: word_less_nat_alt)
+    have ptr_eq: "head +\<^sub>p uint idx = head +\<^sub>p int (unat idx)"
+      by (simp only: uint_nat)
+    show "IS_VALID(32 word) s (head +\<^sub>p uint idx)"
+      apply (subst ptr_eq)
+      by (rule head_valid[where st' = s and h = "unat idx", OF _ idx_nat_lt])
+         simp
+  qed
+  have init_src:
+    "(whileLoop (\<lambda>(idx :: 32 word) st. idx < 0x10000)
+      (\<lambda>idx. do {
+          guard (\<lambda>st. IS_VALID(32 word) st (head +\<^sub>p uint idx));
+          modify (heap_w32_update
+            (\<lambda>h. h(head +\<^sub>p uint idx := no_entry32)));
+          return (idx + 1)
+        }) (0 :: 32 word) :: (32 word, lifted_globals) res_monad) \<bullet> s
+    \<lbrace> \<lambda>r t. r = Result (0x10000 :: 32 word)
+          \<and> heap_typing t = heap_typing s
+          \<and> (\<forall>h < hash_size. heap_w32 t (head +\<^sub>p int h) = no_entry32)
+          \<and> heap_bytes t src (unat src_len) = ?src_bytes \<rbrace>"
+    by (rule build_index_head_init_loop[
+        where src = src and n = "unat src_len", OF head_valid_word])
+  have init_buf:
+    "(whileLoop (\<lambda>(idx :: 32 word) st. idx < 0x10000)
+      (\<lambda>idx. do {
+          guard (\<lambda>st. IS_VALID(32 word) st (head +\<^sub>p uint idx));
+          modify (heap_w32_update
+            (\<lambda>h. h(head +\<^sub>p uint idx := no_entry32)));
+          return (idx + 1)
+        }) (0 :: 32 word) :: (32 word, lifted_globals) res_monad) \<bullet> s
+    \<lbrace> \<lambda>r t. r = Result (0x10000 :: 32 word)
+          \<and> heap_typing t = heap_typing s
+          \<and> (\<forall>h < hash_size. heap_w32 t (head +\<^sub>p int h) = no_entry32)
+          \<and> heap_bytes t buf n = heap_bytes s buf n \<rbrace>"
+    by (rule build_index_head_init_loop[
+        where src = buf and n = n, OF head_valid_word])
+  have init:
+    "(whileLoop (\<lambda>(idx :: 32 word) st. idx < 0x10000)
+      (\<lambda>idx. do {
+          guard (\<lambda>st. IS_VALID(32 word) st (head +\<^sub>p uint idx));
+          modify (heap_w32_update
+            (\<lambda>h. h(head +\<^sub>p uint idx := no_entry32)));
+          return (idx + 1)
+        }) (0 :: 32 word) :: (32 word, lifted_globals) res_monad) \<bullet> s
+    \<lbrace> \<lambda>r t. r = Result (0x10000 :: 32 word)
+          \<and> heap_typing t = heap_typing s
+          \<and> (\<forall>h < hash_size. heap_w32 t (head +\<^sub>p int h) = no_entry32)
+          \<and> heap_bytes t src (unat src_len) = ?src_bytes
+          \<and> heap_bytes t buf n = heap_bytes s buf n \<rbrace>"
+    using init_src init_buf by (simp add: runs_to_conj)
+  show ?thesis
+  proof (cases "src_len < (4 :: 32 word)")
+    case True
+    show ?thesis
+      unfolding build_index'_def
+      apply runs_to_vcg
+      subgoal
+        apply (rule runs_to_weaken[OF init])
+         apply simp
+        using True
+        apply (simp add: runs_to_iff split: exception_or_result_splits)
+        done
+      done
+  next
+    case False
+    let ?start = "(0xFFFFFFFD + src_len :: 32 word)"
+    have src_long_nat: "min_match \<le> length ?src_bytes"
+      using False by (simp add: word_less_nat_alt min_match_def)
+    have four_le: "4 \<le> unat src_len"
+      using False by (simp add: word_less_nat_alt)
+    have start_unat:
+      "unat ?start = length ?src_bytes - min_match + 1"
+      using build_index_start_unat[OF four_le] by simp
+    have len_src_bytes: "length ?src_bytes = unat src_len"
+      by simp
+    have start_ge:
+      "length ?src_bytes - min_match + 1 \<le> unat ?start"
+      using start_unat by simp
+    have start_le:
+      "unat ?start \<le> length ?src_bytes - min_match + 1"
+      using start_unat by simp
+    have src_len_word_bytes:
+      "length ?src_bytes < unat (no_entry32 :: 32 word)"
+      using src_len_word by simp
+    show ?thesis
+      unfolding build_index'_def
+      apply runs_to_vcg
+      subgoal
+        apply (rule runs_to_weaken[OF init])
+         apply simp
+        subgoal premises prems for t
+        proof -
+          have typing_t: "heap_typing t = heap_typing s"
+            using prems by simp
+          have heads_empty:
+            "\<And>h. h < hash_size \<Longrightarrow>
+              heap_w32 t (head +\<^sub>p int h) = no_entry32"
+            using prems by simp
+          have bytes_t: "heap_bytes t src (unat src_len) = ?src_bytes"
+            using prems by simp
+          have buf_bytes_t: "heap_bytes t buf n = heap_bytes s buf n"
+            using prems by simp
+          have rel_start:
+            "source_index_heap_rel_from t ?src_bytes (unat ?start) head next_arr"
+          proof (rule source_index_heap_rel_from_empty)
+            fix h
+            assume h_lt: "h < hash_size"
+            show "heap_w32 t (head +\<^sub>p int h) = no_entry32"
+              by (rule heads_empty[OF h_lt])
+          next
+            show "length ?src_bytes - min_match + 1 \<le> unat ?start"
+              by (rule start_ge)
+          qed
+          have init_nexts_wf:
+            "source_index_heap_nexts_wf_from t ?src_bytes (unat ?start) next_arr"
+            by (rule source_index_heap_nexts_wf_from_empty[OF start_ge])
+          have init_closed:
+            "source_index_heap_chains_closed_from t ?src_bytes (unat ?start) head next_arr"
+          proof (rule source_index_heap_chains_closed_from_empty)
+            fix h
+            assume h_lt: "h < hash_size"
+            show "heap_w32 t (head +\<^sub>p int h) = no_entry32"
+              by (rule heads_empty[OF h_lt])
+          next
+            show "length ?src_bytes - min_match + 1 \<le> unat ?start"
+              by (rule start_ge)
+          qed
+          have init_inv:
+            "build_index_fill_inv s src src_len ?src_bytes head next_arr ?start t"
+            unfolding build_index_fill_inv_def
+            apply (intro conjI)
+                   apply (rule typing_t)
+                  apply (rule bytes_t)
+                 apply (rule len_src_bytes)
+                apply (rule src_long_nat)
+               apply (rule src_len_word_bytes)
+              apply (rule start_le)
+             apply (rule rel_start)
+            apply (rule init_closed)
+            apply (rule init_nexts_wf)
+            done
+          have fill:
+            "(whileLoop (\<lambda>(i :: 32 word) st. 0 < i)
+              (\<lambda>i. do {
+                p <- return (i - 1);
+                hv <- gets_the (hash4' src p);
+                h <- return (hv && 0xFFFF);
+                guard (\<lambda>s. IS_VALID(32 word) s (next_arr +\<^sub>p uint p));
+                guard (\<lambda>s. IS_VALID(32 word) s (head +\<^sub>p uint h));
+                modify
+                  (heap_w32_update
+                    (\<lambda>a. a(next_arr +\<^sub>p uint p := a (head +\<^sub>p uint h))));
+                modify (heap_w32_update (\<lambda>ha. ha(head +\<^sub>p uint h := p)));
+                return (i - 1)
+              }) ?start :: (32 word, lifted_globals) res_monad) \<bullet> t
+		            \<lbrace>\<lambda>r u. r = Result 0 \<and>
+		              build_index_fill_inv s src src_len ?src_bytes head next_arr 0 u \<and>
+		              heap_bytes u buf n = heap_bytes t buf n\<rbrace>"
+            apply (rule build_index_fill_loop_preserves_inv_heap_bytes[
+                  OF init_inv hashes])
+                   apply (auto intro: head_valid next_valid
+                     dest: head_no_alias next_no_alias next_head_disjoint
+                       head_next_disjoint)
+            apply (metis head_no_alias hash_size_0x10000)
+            done
+	          have fill_generated_inv:
+	            "(whileLoop (\<lambda>(i :: 32 word) st. 0 < i)
+	              (\<lambda>i. do {
+	                hv <- gets_the (hash4' src (i - 1));
+	                guard (\<lambda>s. IS_VALID(32 word) s (next_arr +\<^sub>p uint (i - 1)));
+	                guard (\<lambda>s. IS_VALID(32 word) s (head +\<^sub>p uint (hv && 0xFFFF)));
+	                modify
+	                  (heap_w32_update
+	                    (\<lambda>a. a(next_arr +\<^sub>p uint (i - 1) :=
+	                      a (head +\<^sub>p uint (hv && 0xFFFF)))));
+	                modify
+	                  (heap_w32_update
+	                    (\<lambda>h. h(head +\<^sub>p uint (hv && 0xFFFF) := i - 1)));
+		                return (i - 1)
+		              }) ?start :: (32 word, lifted_globals) res_monad) \<bullet> t
+		            \<lbrace>\<lambda>r u. r = Result 0 \<and>
+		              build_index_fill_inv s src src_len ?src_bytes head next_arr 0 u \<and>
+		              heap_bytes u buf n = heap_bytes t buf n\<rbrace>"
+		            using fill by (simp only: Spec_Monad.return_bind)
+		          have fill_generated:
+	            "(whileLoop (\<lambda>(i :: 32 word) st. 0 < i)
+	              (\<lambda>i. do {
+	                hv <- gets_the (hash4' src (i - 1));
+	                guard (\<lambda>s. IS_VALID(32 word) s (next_arr +\<^sub>p uint (i - 1)));
+	                guard (\<lambda>s. IS_VALID(32 word) s (head +\<^sub>p uint (hv && 0xFFFF)));
+	                modify
+	                  (heap_w32_update
+	                    (\<lambda>a. a(next_arr +\<^sub>p uint (i - 1) :=
+	                      a (head +\<^sub>p uint (hv && 0xFFFF)))));
+	                modify
+	                  (heap_w32_update
+	                    (\<lambda>h. h(head +\<^sub>p uint (hv && 0xFFFF) := i - 1)));
+	                return (i - 1)
+	              }) ?start :: (32 word, lifted_globals) res_monad) \<bullet> t
+	            \<lbrace>\<lambda>r u. r = Result 0 \<and>
+	              heap_typing u = heap_typing s \<and>
+	              heap_bytes u buf n = heap_bytes s buf n\<rbrace>"
+	            apply (rule runs_to_weaken[OF fill_generated_inv])
+	            using buf_bytes_t
+	             apply (clarsimp simp: build_index_fill_inv_def)
+	            done
+          show ?thesis
+            using prems False
+            apply (simp add: runs_to_iff split: exception_or_result_splits)
+            apply (rule runs_to_weaken[OF fill_generated])
+             apply simp
+            done
+        qed
+        done
+      done
+  qed
+qed
+
 lemma unat_suc_word_less:
   fixes n limit :: "32 word"
   assumes "n < limit"
